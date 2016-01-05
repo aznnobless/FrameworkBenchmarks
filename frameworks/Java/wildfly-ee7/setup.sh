db464a3b8013e900e7fed54e8b063d8e0a6c1ae3
blee-techempower <blee@techempower.com>
2016-01-05 12:43:10 -0800

    port check

--- frameworks/Java/wildfly-ee7/setup.sh
+++ frameworks/Java/wildfly-ee7/setup.sh
@@ -9,7 +9,8 @@ echo "=========== 특별출현 ==========="
 ls -al
 
 echo "=========== 포트정검 ==========="
-sudo netstat -anp | grep 8080
+lsof -i tcp:8080
+kill -9 PID
 
 target/wildfly-9.0.1.Final/bin/standalone.sh -b 0.0.0.0 &
 echo "파리새끼 완료!"
