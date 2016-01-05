dae5168db55df39621c648648ae529b88567c6f4
blee-techempower <blee@techempower.com>
2016-01-05 14:22:25 -0800

    AHgit add frameworks/Java/wildfly-ee7/setup.sh

--- frameworks/Java/wildfly-ee7/setup.sh
+++ frameworks/Java/wildfly-ee7/setup.sh
@@ -8,12 +8,12 @@ mvn clean initialize package -Pbenchmark -Ddatabase.host=${DBHOST}
 echo "=========== 특별출현 ==========="
 #ls -al
 
-# echo "=========== 포트정검 ==========="
-# sudo fuser -k 9990/tcp
-# sudo fuser -k 8009/tcp 
-# sudo fuser -k 8080/tcp
-# sudo fuser -k 8443/tcp
-# sudo fuser -k 9993/tcp 
+echo "=========== 포트정검 ==========="
+sudo fuser -k 9990/tcp
+sudo fuser -k 8009/tcp 
+sudo fuser -k 8080/tcp
+sudo fuser -k 8443/tcp
+sudo fuser -k 9993/tcp 
 
 target/wildfly-9.0.1.Final/bin/standalone.sh -b 0.0.0.0 &
 
