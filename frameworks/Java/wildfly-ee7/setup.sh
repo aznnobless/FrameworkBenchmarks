7d171006b31749fb658310f054143371c02f2e43
blee-techempower <blee@techempower.com>
2016-01-05 14:34:07 -0800

    anldknfsalf

--- frameworks/Java/wildfly-ee7/setup.sh
+++ frameworks/Java/wildfly-ee7/setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 echo "파리새끼시작"
 fw_depends java7 maven
 echo "메이븐설치완료"
@@ -9,11 +10,11 @@ echo "=========== 특별출현 ==========="
 #ls -al
 
 echo "=========== 포트정검 ==========="
-sudo fuser -k 9990/tcp
-sudo fuser -k 8009/tcp 
-sudo fuser -k 8080/tcp
-sudo fuser -k 8443/tcp
-sudo fuser -k 9993/tcp 
+sudo fuser -k 9990/tcp &
+sudo fuser -k 8009/tcp &
+sudo fuser -k 8080/tcp &
+sudo fuser -k 8443/tcp &
+sudo fuser -k 9993/tcp &
 
 target/wildfly-9.0.1.Final/bin/standalone.sh -b 0.0.0.0 &
 
