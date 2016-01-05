b443b150db1d9c7499e4b9f86fa765eddfc522b1
blee-techempower <blee@techempower.com>
2016-01-05 14:12:03 -0800

    nah

--- frameworks/Java/wildfly-ee7/setup.sh
+++ frameworks/Java/wildfly-ee7/setup.sh
@@ -6,14 +6,14 @@ export JAVA_OPTS="-Xms2g -Xmx2g -XX:MaxPermSize=256m -XX:+UseG1GC -XX:MaxGCPause
 echo "메이븐 이니셜"
 mvn clean initialize package -Pbenchmark -Ddatabase.host=${DBHOST}
 echo "=========== 특별출현 ==========="
-ls -al
+#ls -al
 
-echo "=========== 포트정검 ==========="
-sudo fuser -k 9990/tcp
-sudo fuser -k 8009/tcp 
-sudo fuser -k 8080/tcp
-sudo fuser -k 8443/tcp
-sudo fuser -k 9993/tcp 
+# echo "=========== 포트정검 ==========="
+# sudo fuser -k 9990/tcp
+# sudo fuser -k 8009/tcp 
+# sudo fuser -k 8080/tcp
+# sudo fuser -k 8443/tcp
+# sudo fuser -k 9993/tcp 
 
 target/wildfly-9.0.1.Final/bin/standalone.sh -b 0.0.0.0 &
 
