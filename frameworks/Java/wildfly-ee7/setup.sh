f462ab1bc806ac92d2968a10d601ade194c74517
blee-techempower <blee@techempower.com>
2016-01-05 14:50:14 -0800

    lunch

--- frameworks/Java/wildfly-ee7/setup.sh
+++ frameworks/Java/wildfly-ee7/setup.sh
@@ -7,6 +7,7 @@ export JAVA_OPTS="-Xms2g -Xmx2g -XX:MaxPermSize=256m -XX:+UseG1GC -XX:MaxGCPause
 echo "메이븐 이니셜"
 mvn clean initialize package -Pbenchmark -Ddatabase.host=${DBHOST}
 echo "=========== 특별출현 ==========="
+echo ${DBHOST}
 #ls -al
 
 echo "=========== 포트정검 ==========="
@@ -15,6 +16,9 @@ sudo fuser -k 8009/tcp &
 sudo fuser -k 8080/tcp &
 sudo fuser -k 8443/tcp &
 sudo fuser -k 9993/tcp &
+
+echo "======== 삽질 ======" 
+
 
 target/wildfly-9.0.1.Final/bin/standalone.sh -b 0.0.0.0 &
 
