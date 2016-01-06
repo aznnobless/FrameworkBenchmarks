b23510a1563699b5ad8732343658cbd0e23030eb
blee-techempower <blee@techempower.com>
2016-01-05 19:16:10 -0800

    Try

--- frameworks/Elixir/phoenix/setup.sh
+++ frameworks/Elixir/phoenix/setup.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
+echo "포닉스 시작"
+
 fw_depends elixir
 
-sed -i 's|localhost|'${DBHOST}'|g' config/prod.exs
+echo "엘릭서 설치 완료"
 
-rm -rf _build deps
+sudo sed -i 's|localhost|'${DBHOST}'|g' config/prod.exs
+
+sudo rm -rf _build deps
 
 export MIX_ENV=prod
 mix local.hex --force
 mix deps.get --force
 mix compile --force
 
+echo "포닉스 서버 시작"
 elixir --detached -S mix phoenix.server
+
+echo "포닉스 셋업파일 끝"
