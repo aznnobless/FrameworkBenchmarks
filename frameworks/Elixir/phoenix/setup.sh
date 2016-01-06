47d070c17041e1185169750f1438d684b69f6f53
blee-techempower <blee@techempower.com>
2016-01-05 19:36:59 -0800

    second test

--- frameworks/Elixir/phoenix/setup.sh
+++ frameworks/Elixir/phoenix/setup.sh
@@ -8,14 +8,18 @@ echo "엘릭서 설치 완료"
 
 sudo sed -i 's|localhost|'${DBHOST}'|g' config/prod.exs
 
+echo "데이터베이스 ::: ${DBHOST}"
+
 sudo rm -rf _build deps
 
+echo "리무브후"
+
 export MIX_ENV=prod
-mix local.hex --force
-mix deps.get --force
-mix compile --force
+sudo mix local.hex --force
+sudo mix deps.get --force
+sudo mix compile --force
 
 echo "포닉스 서버 시작!"
-elixir --detached -S mix phoenix.server
+sudo elixir --detached -S mix phoenix.server
 
 echo "포닉스 셋업파일 끝"
