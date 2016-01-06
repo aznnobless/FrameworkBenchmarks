df669a93fb0a16ccd542a2d9cb5af8cce603edd9
blee-techempower <blee@techempower.com>
2016-01-06 05:13:04 -0800

    wt

--- frameworks/Erlang/cowboy/src/hello_world_app.erl
+++ frameworks/Erlang/cowboy/src/hello_world_app.erl
@@ -13,7 +13,7 @@
 start(_Type, _Args) ->
         crypto:start(),
         application:start(emysql),
-        emysql:add_pool(test_pool, 5000,
+        emysql:add_pool(test_pool, 256,
           "benchmarkdbuser", "benchmarkdbpass", "localhost", 3306,
           "hello_world", utf8),
   emysql:prepare(db_stmt, <<"SELECT * FROM World where id = ?">>),
@@ -24,7 +24,7 @@ start(_Type, _Args) ->
       {"/query", query_handler, []}
     ]}
   ]),
-  {ok, _} = cowboy:start_http(http, 5000, [{port, 8080}], [
+  {ok, _} = cowboy:start_http(http, 256, [{port, 8080}], [
     {env, [{dispatch, Dispatch}]}
   ]),
   hello_world_sup:start_link().
