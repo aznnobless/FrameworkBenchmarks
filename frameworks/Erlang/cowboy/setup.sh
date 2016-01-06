fa4db22eb5f66300ba09b4d737d9a7ad6bfc6601
blee-techempower <blee@techempower.com>
2016-01-06 03:47:31 -0800

    Nothing is free

--- frameworks/Erlang/cowboy/setup.sh
+++ frameworks/Erlang/cowboy/setup.sh
@@ -10,12 +10,18 @@
 #
 #   blee@techempower.com
 #
+echo "카우보이의 시작"
 sed -i 's|"benchmarkdbpass", ".*", 3306|"benchmarkdbpass", "'"${DBHOST}"'", 3306|g' src/hello_world_app.erl
 
+echo "얼렝설치전"
 fw_depends erlang
 
+echo "얼렝 설치완료"
+
 rm -rf deps/* ebin/*
+echo "쓸대없는것 정리후 컴파일시작준비중"
 rebar get-deps
 rebar compile
 
+echo "프레임워크시작"
 erl -pa ebin deps/*/ebin +sbwt very_long +swt very_low -s hello_world -noshell -detached
