6e3e98968ad0d80e136f9d1683a01c37b945dc32
blee-techempower <blee@techempower.com>
2016-01-06 03:26:06 -0800

    Problem solved ready to open a pull request.
    Nothing is free
    
    sofa
    
    try
    
    bibigo
    
    At a home
    
    wt

--- frameworks/Erlang/cowboy/setup.sh
+++ frameworks/Erlang/cowboy/setup.sh
@@ -1,11 +1,33 @@
 #!/bin/bash
 
+# Caution: "line 11 has flaw."
+#   Below sed command will cause another travis-ci test failing in the future.
+#   If you see that, you must doubt this command.
+#   It must be original author's mistake.
+#   I will fix this later with better solution.
+#
+#   dummy change
+#
+#   blee@techempower.com
+#
+echo "카우보이의 시작"
 sed -i 's|"benchmarkdbpass", ".*", 3306|"benchmarkdbpass", "'"${DBHOST}"'", 3306|g' src/hello_world_app.erl
 
+echo "얼렝설치전"
 fw_depends erlang
 
-rm -rf deps/* ebin/*
+echo "얼렝 설치완료"
+
+sudo rm -rf deps/* ebin/*
+echo "쓸대없는것 정리후 컴파일시작준비중"
 rebar get-deps
+echo "디펜던시처리끝 컴파일시작"
 rebar compile
 
-erl -pa ebin deps/*/ebin +sbwt very_long +swt very_low -s hello_world -noshell -detached
+echo "프레임워크시작"
+python --version
+#sudo pip install elasticsearch==1.6.0
+#sudo pip install --upgrade setuptools #Possible try
+echo "=========="
+#erl +K true -pa ebin deps/*/ebin +sbwt very_long +swt very_low -s hello_world -noshell -detached
+erl +K true +sbwt very_long +swt very_low -pa ebin deps/*/ebin -boot start_sasl -s hello_world -noshell -detached
