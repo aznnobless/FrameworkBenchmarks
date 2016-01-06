9fefa10f66466a78d0821235d4313792908e6ec3
blee-techempower <blee@techempower.com>
2016-01-06 05:56:37 -0800

    Fix Erlang/cowboy travis-ci test failing

--- frameworks/Erlang/cowboy/setup.sh
+++ frameworks/Erlang/cowboy/setup.sh
@@ -1,33 +1,22 @@
 #!/bin/bash
 
 # Caution: "line 11 has flaw."
-#   Below sed command will cause another travis-ci test failing in the future.
-#   If you see that, you must doubt this command.
-#   It must be original author's mistake.
-#   I will fix this later with better solution.
+#   Below sed command can cause travis-ci test failing.
+#   Please be aware of testing this on local machine.
+#   If you perform a local test, please apply inverse of   
+#   below command. (hint: ${DBHOST} value must be rollback to
+#   localhost.
 #
-#   dummy change
-#
+#   I will fix this problem later with better solution. 
 #   blee@techempower.com
 #
-echo "카우보이의 시작"
 sed -i 's|"benchmarkdbpass", ".*", 3306|"benchmarkdbpass", "'"${DBHOST}"'", 3306|g' src/hello_world_app.erl
 
-echo "얼렝설치전"
 fw_depends erlang
 
-echo "얼렝 설치완료"
-
 sudo rm -rf deps/* ebin/*
-echo "쓸대없는것 정리후 컴파일시작준비중"
+
 rebar get-deps
-echo "디펜던시처리끝 컴파일시작"
 rebar compile
 
-echo "프레임워크시작"
-python --version
-#sudo pip install elasticsearch==1.6.0
-#sudo pip install --upgrade setuptools #Possible try
-echo "=========="
-#erl +K true -pa ebin deps/*/ebin +sbwt very_long +swt very_low -s hello_world -noshell -detached
-erl +K true +sbwt very_long +swt very_low -pa ebin deps/*/ebin -boot start_sasl -s hello_world -noshell -detached
+erl +K true -pa ebin deps/*/ebin +sbwt very_long +swt very_low -s hello_world -noshell -detached
