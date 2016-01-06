66efe6fc5772307e840cbe9c0a8ebffe6f285f57
blee-techempower <blee@techempower.com>
2016-01-05 16:53:32 -0800

    Works

--- frameworks/Java/wildfly-ee7/setup.sh
+++ frameworks/Java/wildfly-ee7/setup.sh
@@ -7,20 +7,17 @@ echo "메이븐설치완료"
 export JAVA_OPTS=" -Djava.net.preferIPv4Stack=true -Xms2g -Xmx2g -XX:MaxPermSize=256m -XX:+UseG1GC -XX:MaxGCPauseMillis=25 -verbosegc -Xloggc:/tmp/wildfly_gc.log"
 echo "메이븐 이니셜"
 mvn clean initialize package -Pbenchmark -Ddatabase.host=${DBHOST}
-echo "=========== 특별출현 ==========="
-echo ${DBHOST}
-#ls -al
 
-echo "=========== 포트정검 ==========="
-sudo fuser -k 9990/tcp &
-sudo fuser -n tcp -k 8009 &
-sudo fuser -n tcp -k 8080 &
-sudo fuser -k 8443/tcp &
-sudo fuser -k 9993/tcp &
+# echo "=========== 포트정검 ==========="
+# sudo fuser -k 9990/tcp &
+# sudo fuser -n tcp -k 8009 &
+# sudo fuser -n tcp -k 8080 &
+# sudo fuser -k 8443/tcp &
+# sudo fuser -k 9993/tcp &
 
-echo "======== 삽질 ======" 
-#ping 0.0.0.0:8080
-echo "========   ========"
+# echo "======== 삽질 ======" 
+# #ping 0.0.0.0:8080
+# echo "========   ========"
 
 target/wildfly-9.0.1.Final/bin/standalone.sh -b 0.0.0.0 &
 
